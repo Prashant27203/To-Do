f160adb3c6043851a428d9565b1659adf93a373a
Prasahant <prashantst27203@gmail.com>
2023-08-03 03:54:02 +0530

    implemented the crud operations

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,22 +1,52 @@
+import React, { useState } from 'react';
 import './App.css';
-import react from 'react';
-import {useState} from 'react';
+
 function App() {
-const[todoList, setTodoList] = useState([]);
-const [newtodo, setTodo] = useState('');
-const handleChange = (e) => {
-  setTodo (e.target.value);
-} 
-const addTodo = (e) => {
-let arr=[]
-}
+  const [todoList, setTodoList] = useState([]);
+  const [newtodo, setTodo] = useState('');
+
+  const handleChange = (e) => {
+    setTodo (e.target.value);
+  } 
+
+  const addTodo = () => {
+    setTodoList([...todoList, { text: newtodo, completed: false }]);
+    setTodo('');
+  }
+
+  const deleteTodo = (index) => {
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
+  const toggleCompleted = (index) => {
+    const newTodoList = [...todoList];
+    newTodoList[index].completed = !newTodoList[index].completed;
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="App">
-     <div className='addtask'></div>
-     <input onChange={handleChange}/>
-      <button onClick={addTodo}>ADD TASK</button>
-      <div className='List'></div>
-      {newtodo}
+      <div className='header'>
+        <h1>Todo List App</h1>
+      </div>
+      <div className='add-task'>
+        <input onChange={handleChange} value={newtodo} placeholder="Enter a new task" />
+        <button onClick={addTodo}>ADD TASK</button>
+        <button onClick={() => setTodoList([])}>CLEAR ALL</button>
+      </div>
+      <div className='list'>
+        {
+          todoList.map((task, index) => (
+            <div className={`task ${task.completed ? 'completed' : ''}`} key={index}>
+              <h3>{task.text}</h3>
+              <button onClick={() => deleteTodo(index)}>Delete</button>
+              <button onClick={() => toggleCompleted(index)}>Completed</button>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
